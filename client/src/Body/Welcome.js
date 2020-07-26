94422ac0ae61831e5f1e495bcd736735846a5490
gettmure <warcraft6500@gmail.com>
2020-07-26 15:41:48 +1000

    added animation on page load

--- client/src/Body/Welcome.js
+++ client/src/Body/Welcome.js
@@ -7,6 +7,8 @@ import satelliteBackground from '../assets/images/satellite-background.jpg';
 const styles = {
   container: {
     height: '86.6%',
+    overflow: 'hidden',
+    border: '2px solid #559de4',
   },
   welcomeImage: {
     height: '100%',
@@ -15,23 +17,7 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     filter: 'blur(2px)',
-    border: '2px solid #559de4',
-  },
-  welcomeText: {
-    backgroundColor: 'rgba(0,0,0, 0.4)',
-    color: 'white',
-    fontWeight: 'bold',
-    border: '3px solid #f1f1f1',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    zIndex: 2,
-    width: '70%',
-    padding: '20px',
-    textAlign: 'center',
-    textShadow:
-      '2px 0 0 #000, -2px 0 0 #000, 0 2px 0 #000, 0 -2px 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000',
+    transform: 'scale(1.3)',
   },
 };
 
