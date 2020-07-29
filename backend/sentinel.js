6f6d045d506f9d3b3c6f3f6f898aa286081e8734
gettmure <warcraft6500@gmail.com>
2020-07-29 22:43:24 +1000

    fixed wring async png reading via promise

--- backend/sentinel.js
+++ backend/sentinel.js
@@ -1,5 +1,4 @@
 const {
-  S2L2ALayer,
   CRS_EPSG4326,
   ApiType,
   BBox,
@@ -22,6 +21,54 @@ const getDates = (fromDate, toDate) => {
   return [fromDateUtc, toDateUtc];
 };
 
+const getAverageNdvi = async (bbox, fromTime, toTime, layer, imageIndex) => {
+  const getMapParams = {
+    bbox: bbox,
+    fromTime: fromTime,
+    toTime: toTime,
+    width: 1024,
+    height: 720,
+    format: MimeTypes.PNG,
+  };
+  const getBlob = async () => {
+    return await layer.getMap(getMapParams, ApiType.WMS);
+  };
+  const imageBlob = await getBlob();
+  fs.writeFileSync(`./aue${imageIndex}.png`, imageBlob, { encoding: null });
+  const readStream = fs.createReadStream(`./aue${imageIndex}.png`);
+  const pngStream = readStream.pipe(new PNG());
+  const ndviFromStream = new Promise(function (resolve, reject) {
+    pngStream.on('parsed', function () {
+      let sum = 0;
+      let count = 0;
+      for (let y = 0; y < this.height; y++) {
+        for (let x = 0; x < this.width; x++) {
+          let ndvi;
+          const idx = (this.width * y + x) << 2;
+          const red = this.data[idx];
+          const blue = this.data[idx + 2];
+          if (blue + red === 0) {
+            ndvi = 0;
+          } else {
+            ndvi = (blue - red) / (blue + red);
+          }
+          count++;
+          sum += ndvi;
+        }
+      }
+      averageNdvi = sum / count;
+      resolve(averageNdvi);
+    });
+  });
+  const ndvi = await ndviFromStream;
+  const ndviObject = {
+    ndviValue: ndvi,
+    fromTime,
+    toTime,
+  };
+  return ndviObject;
+};
+
 const fetchImages = async (
   minX,
   minY,
@@ -41,46 +88,10 @@ const fetchImages = async (
     maxCloudCoverPercent: cloudiness,
   });
   for (let i = 0; i < 5; i++) {
-    const getMapParams = {
-      bbox: bbox,
-      fromTime: fromTime,
-      toTime: toTime,
-      width: 1024,
-      height: 720,
-      format: MimeTypes.PNG,
-    };
-    console.log(fromTime, toTime);
-    const getBlob = async () => {
-      return await layer.getMap(getMapParams, ApiType.WMS);
-    };
-    const imageBlob = await getBlob();
-    fs.writeFileSync(`./aue${i}.png`, imageBlob, { encoding: null });
+    const ndvi = await getAverageNdvi(bbox, fromTime, toTime, layer, i);
+    ndviArray.push(ndvi);
     fromTime.setFullYear(fromTime.getFullYear() + 1);
     toTime.setFullYear(toTime.getFullYear() + 1);
-    fs.createReadStream(`./aue${i}.png`)
-      .pipe(new PNG())
-      .on('parsed', function () {
-        let sum = 0;
-        let count = 0;
-        for (let y = 0; y < this.height; y++) {
-          for (let x = 0; x < this.width; x++) {
-            let ndvi;
-            const idx = (this.width * y + x) << 2;
-            const red = this.data[idx];
-            const blue = this.data[idx + 2];
-            if (blue + red === 0) {
-              ndvi = 0;
-            } else {
-              ndvi = (blue - red) / (blue + red);
-            }
-            count++;
-            sum += ndvi;
-          }
-        }
-        mediumNdvi = sum / count;
-        console.log(mediumNdvi);
-        ndviArray.push(parseFloat(mediumNdvi.toPrecision(3)));
-      });
   }
   return ndviArray;
 };
